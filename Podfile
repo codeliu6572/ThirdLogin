8424b625db9955c399fbb96076ffa21b210a2de0
codeliu6572 <codeliu6572@163.com>
2016-09-10 10:11:04 +0800

    ThirdLogin
    新浪微博第三方登录

---
+++ Podfile
@@ -0,0 +1,38 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+
+# 主模块(必须)
+ pod 'ShareSDK3'
+ # Mob 公共库(必须) 如果同时集成SMSSDK iOS2.0:可看此注意事项：http://bbs.mob.com/thread-20051-1-1.html
+ pod 'MOBFoundation'
+ 
+ # UI模块(非必须，需要用到ShareSDK提供的分享菜单栏和分享编辑页面需要以下1行)
+ pod 'ShareSDK3/ShareSDKUI'
+ 
+ # 平台SDK模块(对照一下平台，需要的加上。如果只需要QQ、微信、新浪微博，只需要以下3行)
+ pod 'ShareSDK3/ShareSDKPlatforms/QQ'
+ pod 'ShareSDK3/ShareSDKPlatforms/SinaWeibo'
+ pod 'ShareSDK3/ShareSDKPlatforms/WeChat'
+ 
+ pod 'ShareSDK3/ShareSDKPlatforms/RenRen'
+ pod 'ShareSDK3/ShareSDKPlatforms/AliPaySocial'
+ pod 'ShareSDK3/ShareSDKPlatforms/Kakao'
+
+target '第三方登录' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for 第三方登录
+
+  target '第三方登录Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target '第三方登录UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- 第三方登录/ViewController.m
+++ 第三方登录/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <ShareSDKExtension/SSEThirdPartyLoginHelper.h>
 @interface ViewController ()
 
 @end
@@ -17,8 +17,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 100, 100, 100);
+    btn.backgroundColor = [UIColor orangeColor];
+    [btn addTarget:self action:@selector(btnAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 }
 
+
+- (void)btnAction
+{
+    //例如QQ的登录
+    [ShareSDK getUserInfo:SSDKPlatformTypeSinaWeibo
+           onStateChanged:^(SSDKResponseState state, SSDKUser *user, NSError *error)
+     {
+         if (state == SSDKResponseStateSuccess)
+         {
+             
+             NSLog(@"uid=%@",user.uid);
+             NSLog(@"%@",user.credential);
+             NSLog(@"token=%@",user.credential.token);
+             NSLog(@"nickname=%@",user.nickname);
+         }
+         
+         else
+         {
+             NSLog(@"%@",error);
+         }
+         
+     }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
